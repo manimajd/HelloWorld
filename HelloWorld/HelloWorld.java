d8cc0412c4b9df90c560710ef02c188946c8b8e6
Mani Majd <mani.majd@gmail.com>
2018-06-16 19:32:07 -0700

    Add files via upload

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,40 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println();
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("|        |");
+		System.out.println(" \\______/");
+		
+		System.out.println();
+		
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("|  STOP  |");
+		System.out.println(" \\______/");
+		
+		System.out.println();
+		
+		System.out.println("  ______");
+		System.out.println(" /      \\");
+		System.out.println("+--------+");
+		System.out.println(" \\______/");
+		
+		System.out.println();
+		
+
+		System.out.println(" \\______/");
+		System.out.println("+--------+");
+
+		
+		
+		
+	
+	}
+
+
+}
